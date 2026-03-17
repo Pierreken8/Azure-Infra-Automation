4a4456f168fa321c39c2684f7504da640e24655c
LievinKP <71077602+Pierreken8@users.noreply.github.com>
2026-03-17 13:11:33 +0100

    Update main.tf

--- Environments/dev/main.tf
+++ Environments/dev/main.tf
@@ -1,1 +1,10 @@
 
+module "network_test" {
+  source              = "../../Modules/Vnet"
+  vnet_name           = "VNET-HOGENT-TEST"
+  address_space       = "10.0.0.0/16"
+  location            = "West Europe"
+  resource_group_name = "SyncProjectSchoon"
+  subnet_name         = "Subnet-Leerlingen"
+  subnet_prefix       = "10.0.1.0/24"
+}
